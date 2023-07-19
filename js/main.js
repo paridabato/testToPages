e2f283afbea32dd0d3a56f32033f2fb66db021e4
Ivan Herasymenko <1315.ig@gail.com>
2023-07-19 19:07:46 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -40,12 +40,12 @@ window.addEventListener('DOMContentLoaded', () => {
     // =========== Gsap ============= //
 
     gsap.registerPlugin(ScrollTrigger);
- 
 
 
-      
-         
-        
+
+
+
+
     gsap.to(".hero-img-1", {
         scrollTrigger: {
             start: "top bottom",
@@ -80,11 +80,11 @@ window.addEventListener('DOMContentLoaded', () => {
             gsap.set(".panel", { zIndex: (i, target, targets) => targets.length - i });
 
             var images = gsap.utils.toArray('.panel:not(.img-5)');
-        
+
             images.forEach((image, i) => {
-        
+
                 var tl = gsap.timeline({
-        
+
                     scrollTrigger: {
                         trigger: " .wrapper",
                         start: () => "top -" + (window.innerHeight * (i + 0.1)),
@@ -92,30 +92,30 @@ window.addEventListener('DOMContentLoaded', () => {
                         scrub: 1,
                     }
                 })
-        
+
                 tl.to(image, { "clip-path": "inset(0 0 200% 0)", ease: "power1.inOut", });
-        
+
             });
-        
-        
-         
+
+
+
                 gsap.set(".panel-text", { zIndex: (i, target, targets) => targets.length - i });
-        
+
                 const texts = gsap.utils.toArray('.panel-text');
-        
+
                 texts.forEach((text, i) => {
-        
+
                     var tl = gsap.timeline({
-        
+
                         scrollTrigger: {
                             trigger: ".wrapper",
                             start: () => "top -" + (window.innerHeight * i),
                             end: () => "+=" + window.innerHeight,
                             scrub: 1,
                         }
-        
+
                     })
-        
+
                     gsap
                         .from(text, {
                             scrollTrigger: {
@@ -125,13 +125,198 @@ window.addEventListener('DOMContentLoaded', () => {
                             },
                             duration: 1, y: 50,
                         })
-        
+
                 });
 
         },
 
     }
     )
+
+})
+
+$(document).ready(function () {
+    const currentButton = $('.partnership-programs-plans__btn-container .btn-base')
+    currentButton.hover(function () {
+
+
+        if ($(this).hasClass('btn-base-small')) {
+            $(this).addClass('is-active')
+            $(this).removeClass('btn-base-small')
+            $(this).addClass('btn-base-lg')
+        }
+
+        $(this).siblings($('.btn-base')).removeClass('is-active')
+        $(this).siblings($(".btn-base")).removeClass('btn-base-lg')
+        $(this).siblings($(".btn-base")).addClass('btn-base-small')
+    });
+
+
+    $('.owl-carousel-plans').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: true,
+                stagePadding: 0,
+                URLhashListener: true,
+                autoplayHoverPause: true,
+                startPosition: "URLHash",
+            },
+            900: {
+                items: 2,
+                nav: false,
+            }
+        }
+    })
+
+
+    $('.owl-carousel-awards').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+
+        responsive: {
+            0: {
+                items: 2
+            },
+            575: {
+                items: 2,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: true,
+                stagePadding: 0,
+                URLhashListener: true,
+                autoplayHoverPause: true,
+                startPosition: "URLHash",
+            },
+
+        }
+    })
+
+
+    $('.owl-carousel-features').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        dots: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        stagePadding: 0,
+        URLhashListener: true,
+        autoplayHoverPause: true,
+        startPosition: "URLHash",
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+            },
+            600: {
+                items: 1,
+                dots: true,
+            },
+
+        },
+
+    })
+
+
+    function collapsed() {
+        $(".card-header h2").find("button").addClass('collapsed')
+    }
+    collapsed()
+
+})
+
+$(document).ready(function () {
+    // plans carousel//
+    $('.owl-carousel-price').owlCarousel({
+        loop: false,
+        margin: 30,
+        stagePadding: 50,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 0,
+                margin: 10,
+            },
+            600: {
+                items: 1
+            },
+
+        }
+    })
+
+    // plans benefits//
+    $('.owl-carousel-benefits').owlCarousel({
+        loop: false,
+        margin: 20,
+        stagePadding: 50,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 0,
+                margin: 10,
+            },
+            600: {
+                items: 1
+            },
+
+        }
+    })
+
+    // plans benefits//
+    $('.owl-carousel-why').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 20,
+            },
+            600: {
+                items: 1,
+                nav: false,
+            },
+
+        }
+    })
+
+
+    //splide//
+    new Splide('#splide-first', {
+        type: 'loop',
+        drag: 'free',
+        focus: 'center',
+        perPage: 3,
+        pagination: false,
+        arrows: false,
+        dots: false,
+        autoScroll: {
+            speed: 1,
+        },
+        breakpoints: {
+
+            575: {
+                perPage: 2,
+            },
+            320: {
+                perPage: 1,
+            },
+        }
+    }).mount(window.splide.Extensions);
 
 })
 
